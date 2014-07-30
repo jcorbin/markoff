fefb5462f806fc68ebde62f9b5aa8fd24e463ca1
Joshua T Corbin <jcorbin@wunjo.org>
2014-07-29 23:25:34 -0700

    Drop vim license test

--- test/markov.js
+++ test/markov.js
@@ -1,5 +1,3 @@
-var test = require('tape');
-
 var Markov = require('../markov');
 var createTestObjects = require('./object');
 
@@ -321,99 +319,6 @@ markovTest('Build a 2-markov', {
         }
 
     ]);
-
-    assert.end();
-});
-
-var Corpus = {
-    vimLicense: [
-        'there are no restrictions on distributing unmodified copies of vim except that they must include this license text',
-        'you can also distribute unmodified parts of vim likewise unrestricted except that they must include this license text',
-        'you are also allowed to include executables that you made from the unmodified vim sources plus your own usage examples and vim scripts',
-        'it is allowed to distribute a modified version of vim including executables and source code when the following four conditions are met',
-        'this license text must be included unmodified',
-        'the modified vim must be distributed in one of the following five ways',
-        'if you make changes to vim yourself you must clearly describe in the distribution how to contact you',
-        'when the maintainer asks you  for a copy of the modified vim you distributed you must make your changes including source code available to the maintainer without fee',
-        'the maintainer reserves the right to include your changes in the official version of vim',
-        'what the maintainer will do with your changes and under what license they will be distributed is negotiable',
-        'if there has been no negotiation then this license or a later version also applies to your changes',
-        'the current maintainer is bram moolenaar',
-        'if this changes it will be announced in appropriate places',
-        'when it is completely impossible to contact the maintainer the obligation to send him your changes ceases',
-        'once the maintainer has confirmed that he has received your changes they will not have to be sent again',
-        'if you have received a modified vim that was distributed as mentioned under',
-        'you are allowed to further distribute it unmodified as mentioned at',
-        'if you make additional changes the text under a applies to those changes',
-        'provide all the changes including source code with every copy of the modified vim you distribute',
-        'this may be done in the form of a context diff',
-        'you can choose what license to use for new code you add',
-        'the changes and their license must not restrict others from making their own changes to the official version of vim',
-        'when you have a modified vim which includes changes as mentioned under',
-        'you can distribute it without the source code for the changes if the following three conditions are met',
-        'the license that applies to the changes permits you to distribute the changes to the vim maintainer without fee or restriction and permits the vim maintainer to include the changes in the official version of vim without fee or restriction',
-        'you keep the changes for at least three years after last distributing the corresponding modified vim',
-        'when the maintainer or someone who you distributed the modified vim to asks you for the changes within this period you must make them available to him',
-        'you clearly describe in the distribution how to contact you',
-        'this contact information must remain valid for at least three years after last distributing the corresponding modified vim or as long as possible',
-        'when the gnu general public license applies to the changes you can distribute the modified vim under the gnu gpl version 2 or any later version',
-        'a message must be added at least in the output of the version command and in the intro screen such that the user of the modified vim is able to see that it was modified',
-        'if you distribute a modified version of vim you are encouraged to use the vim license for your changes and make them available to the maintainer including the source code',
-        'the preferred way to do this is by email or by uploading the files to a server and emailing the url',
-        'if the number of changes is small emailing a context diff will do',
-        'it is not allowed to remove this license from the distribution of the vim sources parts of it or from a modified version',
-        'you may use this license for previous vim releases instead of the license that they came with at your option',
-    ]
-};
-
-function rotatingSequence(seq) {
-    var i = 0;
-    var self = function() {
-        var r = seq[i];
-        i = (i + 1) % seq.length;
-        return r;
-    };
-    self.reset = function() {
-        i = 0;
-    };
-    self.set = function(j) {
-        i = j;
-    };
-    return self;
-}
-
-test('Markov chain', function(assert) {
-    var seq = rotatingSequence([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
-
-    var markov = new Markov();
-    Corpus.vimLicense.forEach(function(sentence) {
-        markov.addTokens(sentence.split(/\s+/));
-    });
-
-    assert.equal(
-        markov.chain(5, null, seq).join(' '),
-        'if the gnu general public'
-    );
-    seq.reset();
-
-    assert.equal(
-        markov.chain(10, null, seq).join(' '),
-        'if the gnu general public license that was modified'
-    );
-    seq.reset();
-
-    seq.set(4);
-    assert.equal(
-        markov.chain(10, null, seq).join(' '),
-        'the output of vim you are also allowed to include'
-    );
-    seq.reset();
-
-    assert.equal(
-        markov.chain(10, markov.createState('what'), seq).join(' '),
-        'license for at'
-    );
-    seq.reset();
 
     assert.end();
 });
